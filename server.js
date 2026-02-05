c6f55490e0a44de80ce2e7fe743ae7b0f72768a2
Shubham <shubhamlade495@gmail.com>
2026-02-05 23:12:24 +0530

    Final deployment

--- server.js
+++ server.js
@@ -1,18 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://googledrive-frontend-swart.vercel.app"
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true
-}));
-
-app.options("*", cors());
-
 require("dotenv").config();
 
 const connectDB = require("./src/config/db");
@@ -20,11 +7,20 @@ const authRoutes = require("./src/routes/auth.routes");
 const driveRoutes = require("./src/routes/drive.routes");
 
 const app = express();
-
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://googledrive-frontend-swart.vercel.app",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+app.options("*", cors());
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
-// connect DB
+app.use(express.urlencoded({ extended: true }));
 connectDB();
 app.use("/api/auth", authRoutes);
 app.use("/api/drive", driveRoutes);
@@ -34,4 +30,6 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`✅ Server running on port ${PORT}`);
+});
